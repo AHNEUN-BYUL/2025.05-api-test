b4ad0c74f2a55d02bacbb93affa713cf11ffe3d2
eunbyul.ahn <ahneb@rsp-corp.com>
2025-05-12 10:50:41 +0900

    Fix: 改行更新及び説明追加

--- test.js
+++ test.js
@@ -1,26 +1,36 @@
-let port;
-let reader;
-let writer;
-let keepReading = true;
+/**
+ * TODO. navigator 参照
+ * https://developer.mozilla.org/ja/docs/Web/API/Serial
+ * Web Serial API の Serial インターフェイスは、
+ * ウェブページがシリアルポートを検出し、接続するためのプロパティやメソッドを提供します。
+ * 
+ */
 
+let port;               // シリアルポート
+let reader;             // データ受信
+let writer;             // データ送信
+let keepReading = true; // 解除までにデータを受信する
+
+// 《ポート連結》ボタン押下した時のイベント
 document.getElementById("connect").addEventListener("click", async () => {
     try {
-    port = await navigator.serial.requestPort();
-    await port.open({ baudRate: 9600 });
+        port = await navigator.serial.requestPort();
+        await port.open({ baudRate: 9600 });
 
-    writer = port.writable.getWriter();
-    reader = port.readable.getReader();
+        writer = port.writable.getWriter();
+        reader = port.readable.getReader();
 
-    keepReading = true;
-    readLoop();
+        keepReading = true;
+        readLoop();
 
-    document.getElementById("disconnect").disabled = false;
-    console.log("ポート連結済み");
+        document.getElementById("disconnect").disabled = false;
+        console.log("ポート連結済み");
     } catch (err) {
-    console.error("連結エラー:", err);
+        console.error("連結エラー:", err);
     }
 });
 
+// 《連結解除》ボタン押下した時のイベント
 document.getElementById("disconnect").addEventListener("click", async () => {
     keepReading = false;
     if (reader) await reader.cancel();
@@ -30,22 +40,24 @@ document.getElementById("disconnect").addEventListener("click", async () => {
     console.log("ポート連結解除済み");
 });
 
+// 《転送》ボタン押下した時のイベント
 document.getElementById("send").addEventListener("click", async () => {
     const text = document.getElementById("sendText").value;
     const encoder = new TextEncoder();
     await writer.write(encoder.encode(text + "\r\n")); // 計量器終了文字注意
 });
 
+
 async function readLoop() {
     const decoder = new TextDecoder();
     while (keepReading) {
-    try {
-        const { value, done } = await reader.read();
-        if (done) break;
-        document.getElementById("receive").value += decoder.decode(value);
-    } catch (error) {
-        console.error("受信エラー:", error);
-        break;
-    }
+        try {
+            const { value, done } = await reader.read();
+            if (done) break;
+            document.getElementById("receive").value += decoder.decode(value);
+        } catch (error) {
+            console.error("受信エラー:", error);
+            break;
+        }
     }
 }
